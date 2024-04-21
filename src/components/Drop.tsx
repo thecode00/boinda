0bd1bef5bc691d0e7cd7654c9e32a12725153fdb
thecode00 <thecode0087@gmail.com>
2024-04-22 02:28:09 +0900

    Add style

---
+++ src/components/Drop.tsx
@@ -0,0 +1,67 @@
+import React, { useCallback, useMemo } from "react";
+import { useDropzone } from "react-dropzone";
+
+const baseStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
+
+const focusedStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+function Drop() {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // TODO: csv가 아닌 파일 드랍시 gsap으로 애니메이션
+    if (acceptedFiles.length > 0) {
+      console.log(acceptedFiles);
+    }
+  }, []);
+
+  const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } =
+    useDropzone({
+      onDrop,
+      accept: {
+        "text/csv": [".csv"],
+      },
+    });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isFocused ? focusedStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  );
+
+  return (
+    <section className="container">
+      <div {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+    </section>
+  );
+}
+
+export default Drop;
