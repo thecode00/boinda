5cb8b1cd0c49628e554c6f934973bfe3ee963972
thecode00 <thecode0087@gmail.com>
2024-05-22 15:06:00 +0900

    Edit dynamic package download

--- src/wasm/main.py
+++ src/wasm/main.py
@@ -1,10 +1,12 @@
 
+import time
 import js
 import io
 from pyodide.ffi import create_proxy
 import pyodide_js
 
 is_package_downloaded = False
+df = None
 
 
 async def install_packages():  # 필요한 패키지를 동적으로 다운로드
@@ -21,19 +23,22 @@ async def get_data(event):
     if not is_package_downloaded:
         await install_packages()
         is_package_downloaded = True
+    start = time.time()
 
-    file = event.detail.file
+    file = event.detail.data
 
     fr = js.FileReader.new()
-    fr.onload = lambda e: process_csv(e.target.result)
+    fr.onload = lambda e: print("time :", time.time() - start)
     fr.readAsText(file)
-    print(dir(js.FileReader.new()))
     # print("File received:", file, type(file), dir(file))
 
     # # CSV 데이터를 처리하는 함수
-    def process_csv(data):
-        # pandas를 사용하여 CSV 데이터 읽기
-        df = pd.read_csv(io.StringIO(data))
+
+
+def process_csv(data):
+    global df
+    # pandas를 사용하여 CSV 데이터 읽기
+    df = pd.read_csv(io.StringIO(data))
 
 
 get_data_proxy = create_proxy(get_data)
