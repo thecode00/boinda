07a38cda26be823d25a66ac2125ca281d771a396
thecode00 <thecode0087@gmail.com>
2024-05-22 15:07:44 +0900

    Remove not used code

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import CsvPage from "./pages/CsvPage";
 import Root from "./pages/Root";
-import { useEffect } from "react";
-
 const browserRouter = createBrowserRouter([
   {
     path: "/",
--- src/components/LinePlot.tsx
+++ src/components/LinePlot.tsx
@@ -1,10 +1,21 @@
-import * as d3 from "d3";
-import { useRef, useEffect } from "react";
 import { csvState } from "./Drop";
 import { useRecoilValue } from "recoil";
+import * as d3 from "d3";
+import { useEffect, useRef } from "react";
 
-export default function LinePlot({}) {
-  const data = useRecoilValue(csvState)[0];
+export default function LinePlot({
+  width = 640,
+  height = 400,
+  marginTop = 20,
+  marginRight = 20,
+  marginBottom = 20,
+  marginLeft = 20,
+}) {
+  const svgRef = useRef(null);
+  const data = useRecoilValue(csvState);
   console.log(data);
-  return <></>;
+
+  const x = d3.scaleLinear();
+
+  return <svg ref={svgRef}></svg>;
 }
