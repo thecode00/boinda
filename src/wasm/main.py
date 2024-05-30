3d68dfca070cf905fb2f1c79e60218549123356a
thecode00 <thecode0087@gmail.com>
2024-05-31 02:38:47 +0900

    Edit global variable

--- src/wasm/main.py
+++ src/wasm/main.py
@@ -1,6 +1,5 @@
 
 import string
-import time
 import js
 import io
 from pyodide.ffi import create_proxy
@@ -10,13 +9,9 @@ is_package_downloaded = False
 df = None
 
 
-def dataframe_to_csv(data):
-    return df.to_csv(data)
-
-
 def data_to_js(data, key: string):  # 데이터는 항상 문자열로
-    print("data_to_pyscript", data)
-    js.jsStore._notify("test", data)
+    print("data_to_js", data)
+    js.jsStore.getDataFromPyscript(key, data)
 
 
 async def install_packages():  # 필요한 패키지를 동적으로 다운로드
@@ -33,40 +28,44 @@ async def get_data(event):
     if not is_package_downloaded:
         await install_packages()
         is_package_downloaded = True
-    start = time.time()
 
     file = event.detail.data
 
     fr = js.FileReader.new()
-    fr.onload = lambda e: print("time :", time.time() - start)
+    fr.onload = lambda e: process_csv(e.target.result)
     fr.readAsText(file)
-    # print("File received:", file, type(file), dir(file))
-
-    # # CSV 데이터를 처리하는 함수
 
 
 def process_csv(data):
     global df
     # pandas를 사용하여 CSV 데이터 읽기
     df = pd.read_csv(io.StringIO(data))
+    data_to_js(get_csv_type_info(), "type")
 
 
 def get_csv_type_info():
-    if not df:
-        return ""
+    global df
+    try:
+        buffer = io.StringIO()
+        df.info(buf=buffer)
+        info_str = buffer.getvalue()
+        lines = [line.split() for line in info_str.splitlines()[3:-2]]
 
-    return dataframe_to_csv(df.info())
+        return pd.DataFrame(lines).to_csv()
+    except Exception as e:
+        print("Error : ", e)
+        return "Error" + str(e)
 
 
 end_point = {
     "file": get_data,
-    "getType": data_to_js(get_csv_type_info(), "type"),
+    "getType": lambda: data_to_js(get_csv_type_info(), "type"),
 }
 
 
-def store(data):  # 자바스크립트와의 통신을 담당하는 함수
+async def store(data):  # 자바스크립트와의 통신을 담당하는 함수
     if data.detail.key in end_point:
-        end_point[data.detail.key](data)
+        await end_point[data.detail.key](data)
 
 
 get_data_proxy = create_proxy(store)
